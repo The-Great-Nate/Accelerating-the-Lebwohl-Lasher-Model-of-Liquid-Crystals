d475f4cf5fabefe8fd74ea69fe156a9f057385a2
The-Great-Nate <nathant1388@gmail.com>
2025-10-31 03:50:39 +0000

    WIll test this MPI implentation on main pc

---
+++ MakeTestConfig.py
@@ -0,0 +1,31 @@
+import sys
+import numpy as np
+#=======================================================================
+def initdat(nmax):
+    """
+    Arguments:
+      nmax (int) = size of lattice to create (nmax,nmax).
+    Description:
+      Function to create and initialise the main data array that holds
+      the lattice.  Will return a square lattice (size nmax x nmax)
+	  initialised with random orientations in the range [0,2pi].
+	Returns:
+	  arr (float(nmax,nmax)) = array to hold lattice.
+    """
+    arr = np.random.random_sample((nmax,nmax))*2.0*np.pi
+    return arr
+#======================================================================
+def main(name, size):
+    np.savetxt(f"{name}.txt", initdat(size))
+    print(f"Saved to {name}.txt!\tHave a nice day!")
+    return
+#======================================================================
+if __name__ == '__main__':
+    if int(len(sys.argv)) == 5:
+        PROGNAME = sys.argv[0]
+        NAME = int(sys.argv[1])
+        SIZE = int(sys.argv[2])
+        main(NAME, SIZE)
+    else:
+        print("Usage: python {} <NAME> <SIZE>".format(sys.argv[0]))
+#=======================================================================
--- test_various_procs.sh
+++ test_various_procs.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-procs=(1 2 4 6 8 12 14)
+procs=(1 2 4 6 8)
 
-# This was running on an I9-12900H system with 14 cores in total, modify if needed
+# This was running on an Ryzen 7 9800X3D system with 14 cores in total, modify if needed
 for nproc in "${procs[@]}"; do
-    echo "-------------------------------------------"
-    echo "Running with $nproc MPI process(es)..."
-    echo "-------------------------------------------"
+    echo "======================== Running with $nproc MPI process(es)... ========================"
+    for ((i=0; i<20; i++)); do
+        echo "----------------------- $i 'th-teration "-----------------------
 
-    # Run and save output
-    mpiexec -n $nproc python LebwohlLasher_MPI.py 100 100 5.6725 0
+        # Run and save output
+        mpiexec -n $nproc python LebwohlLasher_MPI.py 100 100 0.2 0 Test_Config.txt
 
-    echo "Done with $nproc processes."
+    done
+    echo "======================== Done with $nproc process(es). ========================"
 done
+
+echo "==========================================="
+
+for ((i=0; i<100; i++)); do
+    echo "-------------------------------------------"
+    echo "Running with Original Serial"
+    echo "-------------------------------------------"
+
+    python LebwohlLasher.py 100 100 0.2 0 Test_Config.txt
+done
+echo "Done with original serial code"
