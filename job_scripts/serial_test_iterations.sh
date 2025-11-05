11b54e036ccbf8d157c6bec4349730cad725fa58
Nate <58391082+The-Great-Nate@users.noreply.github.com>
2025-11-05 03:17:47 +0000

    Corrected test cases and scenario used for report

--- job_scripts/serial_test_iterations.sh
+++ job_scripts/serial_test_iterations.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sizes=(25 50 100 200 500 1000)
+sizes=(25 50 100 200)
 
 for size in "${sizes[@]}"; do
-    for ((i=0; i<100; i++)); do
+    for ((i=0; i<20; i++)); do
         echo "-------------------------------------------"
         echo "Running with Original Serial"
         echo "-------------------------------------------"
 
-        python programs/LebwohlLasher.py 500 $size 0.2 0 Test_Configs/Test_Config_$size.txt
+        python programs/LebwohlLasher.py 250 $size 0.2 0 Test_Configs/Test_Config_$size.txt
     done
 done
 echo "Done with original serial code"
