0befd53cef549dececedd2941df609a86fb7c41f
The-Great-Nate <nathant1388@gmail.com>
2025-11-05 07:16:11 +0000

    Fixed run file as I cant read arguments and I coded for too long

--- programs/run_LL_Cython_OpenMP.py
+++ programs/run_LL_Cython_OpenMP.py
@@ -13,39 +13,33 @@ if __name__ == '__main__':
     ITERATIONS = int(sys.argv[1])
     SIZE = int(sys.argv[2])
     TEMPERATURE = float(sys.argv[3])
-
+    PLOTFLAG = int(sys.argv[4])
+    print(sys.argv[5])
     # Default values
-    PLOTFLAG = 0
-    FILE = None
+    FILE = "0"
     THREADS = 1
-
-    ### Determine meaning of the 4th argument (after temperature) ###
-    if n_args >= 5:
-        arg4 = sys.argv[4]
-        if arg4.isdigit():
-            THREADS = int(arg4)        # Treat as THREADS
-        else:
-            PLOTFLAG = int(arg4)       # Treat as PLOTFLAG
-
-    # ### 6th argument: could be file or plotflag depending on what we already have ###
-    if n_args >= 6:
-        if FILE is None and not sys.argv[5].isdigit():
-            FILE = sys.argv[5]
-        elif PLOTFLAG == 0 and sys.argv[5].isdigit():
-            PLOTFLAG = int(sys.argv[5])
-        elif THREADS == 1 and sys.argv[5].isdigit():
-            THREADS = int(sys.argv[5])
 
     # ### 7th argument: only possible if both plotflag and file are given ###
     if n_args >= 7:
-        if FILE is None and not sys.argv[6].isdigit():
-            FILE = sys.argv[6]
-        elif THREADS == 1 and sys.argv[6].isdigit():
-            THREADS = int(sys.argv[6])
+        print("I GOT HERE")
+        FILE = sys.argv[5]
+        THREADS = int(sys.argv[6])
+
+    # ### 6th argument: could be file or plotflag depending on what we already have ###
+    elif n_args >= 6:
+        if not sys.argv[5].isdigit():
+            FILE = sys.argv[5]
+        else:
+            THREADS = int(sys.argv[5])
+
+
 
     # ### Call main with correct arguments ###
-    if FILE is None:
-        LL_C_OP.main(PROGNAME, ITERATIONS, SIZE, TEMPERATURE, PLOTFLAG)
+    if FILE == "0":
+        print("File == 0")
+        LL_C_OP.main(PROGNAME, ITERATIONS, SIZE, TEMPERATURE, PLOTFLAG, threads = THREADS)
     else:
+        print("FILE IS REAL,")
+        print(PLOTFLAG)
         LL_C_OP.main(PROGNAME, ITERATIONS, SIZE, TEMPERATURE, PLOTFLAG, FILE, THREADS)
 #=======================================================================
